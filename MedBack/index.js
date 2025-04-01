ef96c9ff0c0cedb2d89ac93d1f416e4b05d85739
abhinav2682004 <abhinav2682004@gmail.com>
2025-04-01 22:24:04 +0530

    Update index.js

--- MedBack/index.js
+++ MedBack/index.js
@@ -19,11 +19,19 @@ app.use(express.json());
 app.use(cors());
 const mongoose=require('mongoose')
 console.log(process.env.MONGO_URI)
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(()=>console.log("Successful connection"));
-
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+connectDB()
 app.use('/signup',signUpRoute)
 app.use('/login',loginRoute)
 app.use('/home',homeRoute)
